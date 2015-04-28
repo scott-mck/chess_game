4fcb419886953f4496b69149dc1a35041ceeaa1c
Scott McKenzie <slammayjammay@gmail.com>
2015-04-28 15:10:46 -0700

    Add deep_dup and attr_readers to board

--- board.rb
+++ board.rb
@@ -1,11 +1,17 @@
 require_relative 'piece'
 
 class Board
+  attr_reader :white_pieces, :black_pieces, :grid
+
   def initialize
     @white_pieces = []
     @black_pieces = []
     @grid = Array.new { Array.new(8) }
-    set_up_pieces
+    # set_up_pieces
+  end
+
+  def []=(pos, piece)
+    @grid[pos.first][pos.last] = piece
   end
 
   def set_up_pieces
@@ -35,6 +41,13 @@ class Board
   end
 
   def deep_dup
-    
+    new_board = Board.new
+    @black_pieces.concat(@white_pieces).each do |piece|
+      new_piece = Piece.new(new_board, piece.pos, piece.color)
+      new_board[piece.pos] = new_piece
+      new_board.send("#{piece.color.to_s}_pieces") << new_piece
+    end
+
+    new_board
   end
 end
