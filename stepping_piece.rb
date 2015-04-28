4f2c91588042c0dfa4be0957c730fc1cb99a5d86
Scott McKenzie <slammayjammay@gmail.com>
2015-04-28 13:44:04 -0700

    Start defining SteppingPiece behavior

--- stepping_piece.rb
+++ stepping_piece.rb
@@ -1,4 +1,33 @@
 require_relative 'piece'
 
 class SteppingPiece < Piece
+  def moves
+    moves_array = []
+    hops.each do |direction|
+      new_position = single_step(direction)
+      moves_array += new_position if valid_postion?(new_position)
+    end
+    moves_array
+  end
+end
+
+class Knight < SteppingPiece
+  HOPS = [[1, 2],
+          [1, -2],
+          [-1, 2],
+          [-1, -2],
+          [2, 1],
+          [2, -1],
+          [-2, 1],
+          [-2, -1]]
+
+  def hops
+    HOPS
+  end
+end
+
+class King < SteppingPiece
+  def hops
+    STRAIGHT + DIAGONAL
+  end
 end
