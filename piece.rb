8fd3ea55d7569b61ec8d1b819cee1a6e964920ad
Scott McKenzie <slammayjammay@gmail.com>
2015-04-28 13:42:54 -0700

    Refactor valid_position? and add class constants for directions

--- piece.rb
+++ piece.rb
@@ -1,6 +1,9 @@
 class Piece
   attr_reader :color
 
+  STRAIGHT = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+  DIAGONAL = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+
   def initialize(board, pos, color)
     @board = board
     @pos = pos
@@ -20,11 +23,7 @@ class Piece
   def valid_position?(position)
     return false unless position.all? { |coord| coord.between?(0,7) }
 
-    piece_at_position = @board[position]
-    if piece_at_position.nil?
-      true
-    else
-      piece_at_position.color != @color
-    end
+    return true unless @board.occupied?
+    @color != @board.piece_at(position).color
   end
 end
