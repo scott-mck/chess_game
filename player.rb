e4e91e99337a01a9a96d7216d2b97ea74ee36d0f
Scott McKenzie <slammayjammay@gmail.com>
2015-04-29 13:52:43 -0700

    Refactor Player#input_to_coordinate

--- player.rb
+++ player.rb
@@ -21,22 +21,23 @@ class HumanPlayer
   def get_input
     print "> "
     pair = gets.chomp.split.map { |coord| input_to_coordinate(coord) }
+
     if !@board.occupied?(pair.first)
       raise InvalidInputError.new("Must select a piece at starting position.")
     elsif @board.color_at(pair.first) != @color
       raise InvalidInputError.new("Cannot move other player's pieces")
     end
+
     pair
   end
 
   def input_to_coordinate(code)
     letter, number = code.split('')
-    number = 8 - number.to_i
-    letter = ('a'..'h').to_a.index(letter)
-    [number, letter].each do |coord|
-      if coord.nil? || !coord.between?(0, 7)
-        raise InvalidInputError.new("Invalid coordinates")
-      end
+    row = 8 - number.to_i
+    col = ('a'..'h').to_a.index(letter)
+
+    if [col, row].any? { |coord| coord.nil? || !coord.between?(0, 7) }
+      raise InvalidInputError.new("Invalid coordinates")
     end
   end
 
